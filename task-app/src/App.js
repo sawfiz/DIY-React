04deb859c05f3db4b2b1072a369edca7bfda6457
sawfiz <dashu.up@gmail.com>
2023-04-28 11:19:53 +0800

    Add number to tasks

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -9,8 +9,9 @@ class App extends Component {
     this.state = {
       // Make task an object
       // Add uniqid
-      task: { text: '', id: uniqid() },
+      task: { text: '', id: uniqid(), number: 0 },
       tasks: [],
+      count: 0,
     };
   }
 
@@ -18,20 +19,42 @@ class App extends Component {
     this.setState({
       // How to setState for an object property in state
       // Make sure to include all properties
-      task: { text: event.target.value, id: this.state.task.id },
+      task: {
+        text: event.target.value,
+        id: this.state.task.id,
+        number: this.state.task.number,
+      },
     });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    // const tempArray = this.state.tasks;
-    // tempArray.push(this.state.task);
-    this.setState({
-      task: { text: '', id: uniqid() },
-      // tasks: tempArray,
-      // Use concat to create a new copy of the array with new elment added
-      tasks: this.state.tasks.concat(this.state.task)
-    });
+
+    let tempCount = this.state.count;
+    tempCount++;
+    console.log('🚀 ~ file: App.js:36 ~ App ~ tempCount:', tempCount);
+
+    this.setState(
+      {
+        // Update the task number
+        task: {
+          text: this.state.task.text,
+          id: this.state.task.id,
+          number: tempCount,
+        },
+        count: tempCount,
+      },
+      () =>
+        this.setState(
+          {
+            // Reset the task state
+            task: { text: '', id: uniqid(), number: 0 },
+            // Use concat to create a new copy of the array with new elment added
+            tasks: this.state.tasks.concat(this.state.task),
+          },
+          () => console.log(this.state.tasks)
+        )
+    );
   };
 
   render() {
--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Use function component instead of class component
 function Overview(props) {
   // Remove `this`
   const tasks = props.tasks;
-  const tasksEl = tasks.map((task) => <li key={task.id}>{task.id} {task.text} </li>);
+  const tasksEl = tasks.map((task) => <li key={task.id}>{task.number} {task.text} <button class='button'><i class="fa-regular fa-trash-can"></i></button></li>);
 
   return (
     <div>
